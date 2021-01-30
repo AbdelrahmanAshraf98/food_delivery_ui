d5674e130e5d510109cab4ba2f4e342ad97cd132
Abd ElRahman Ashraf <33667787+AbdelrahmanAshraf98@users.noreply.github.com>
2021-01-30 03:39:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# food_delivery_ui
+ A flutter food delivery app-ui
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
+import '../models/user.dart';
+import '../widgets/recent_orders.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.account_circle),
+            iconSize: 30.0,
+            onPressed: () {},
+          ),
+          title: Center(child: Text('Food Delivery')),
+          actions: <Widget>[
+            FlatButton(
+                onPressed: () {},
+                child: Text(
+                  'Cart(${currentUser.cart.length})',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14.0,
+                  ),
+                )),
+          ],
+        ),
+        body: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 10.0),
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
+                        width: 0.8, color: Theme.of(context).primaryColor),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(width: 0.8),
+                  ),
+                  hintText: 'Search Food or Restaurants',
+                  prefixIcon: Icon(Icons.search, size: 30.0),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ),
+            RecentOrders(),
+          ],
+        ));
+  }
+}
